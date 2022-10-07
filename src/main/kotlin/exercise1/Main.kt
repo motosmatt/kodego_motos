020290a0d2b010f7660b6febcf3915b2f667afd0
Matthew motos <motosmatt13@gmail.com>
2022-10-07 19:53:54 +0800

    add package exercise 1 Main.kt add package exercise 1 Main.kt

--- src/main/kotlin/exercise1/Main.kt
+++ src/main/kotlin/exercise1/Main.kt
@@ -1,4 +1,41 @@
 package exercise1
 
-class Main {
+fun main() {
+
+    var lastName = " "
+    var firstName =" "
+    var middle = " "
+    var address = " "
+    var city = " "
+    var birthdate = " "
+    var state = " "
+    var zip = " "
+    var homePHone = " "
+    var cellPhone = " "
+    var emailAddress = " "
+    var socialSecurityNUmber = " "
+    var isUsCitizen =   " "
+    var hasBeenConvicted = ""
+    var haveDrugScreenTest = " "
+
+
+
+    println("Last name:$lastName")
+    println("First name:$firstName")
+    println("middle: $middle")
+    println("Address: $address")
+    println("City: $city")
+    println("Birthdate: $birthdate")
+    println("State: $state")
+    println("Zip: $zip")
+    println("Home phone: $homePHone")
+    println("Cellphone: $cellPhone")
+    println("Email address: $emailAddress")
+    println("Social security number: $socialSecurityNUmber")
+    println("Are you a U.S citizen? $isUsCitizen")
+    println("Have you ever been convicted of a felony? $hasBeenConvicted")
+    println("If selected for employment are you willing to submit pre-employment drug screening test? $haveDrugScreenTest")
+
+
+
 }
