318081c6255c25606bdf1aa1fbf050531139f7b6
Matthew motos <motosmatt13@gmail.com>
2022-10-10 19:04:50 +0800

    add package exercise 1 Main.kt add package exercise 1 Main.kt

--- src/main/kotlin/exercise1/Main.kt
+++ src/main/kotlin/exercise1/Main.kt
@@ -1,6 +1,15 @@
 package exercise1
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 fun main() {
+
+    var applyingFor:String? = null
+    var desiredSalary:String? = null
+    var workStartDate = LocalDate.parse("2022-10-30")
+    var dateFormat = DateTimeFormatter.ofPattern("mm-dd-yyyy")
+    var formattedWorkStartDate = workStartDate.format(dateFormat)
 
     var lastName = " "
     var firstName =" "
@@ -13,10 +22,10 @@ fun main() {
     var homePHone = " "
     var cellPhone = " "
     var emailAddress = " "
-    var socialSecurityNUmber = " "
-    var isUsCitizen =   " "
-    var hasBeenConvicted = ""
-    var haveDrugScreenTest = " "
+    var socialSecurityNUmber:String? = null
+    var isUsCitizen:String? =null
+    var hasBeenConvicted:String? = null
+    var haveDrugScreenTest:String? = null
 
 
 
@@ -32,7 +41,8 @@ fun main() {
     println("Cellphone: $cellPhone")
     println("Email address: $emailAddress")
     println("Social security number: $socialSecurityNUmber")
-    println("Are you a U.S citizen? $isUsCitizen")
+    println("Are you a U.S citizen? $isUsCitizen" +
+            "")
     println("Have you ever been convicted of a felony? $hasBeenConvicted")
     println("If selected for employment are you willing to submit pre-employment drug screening test? $haveDrugScreenTest")
 
