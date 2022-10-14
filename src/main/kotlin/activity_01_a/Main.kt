74cc95782cd5516d084ec72cd8fd74c4f09019f4
Matthew motos <motosmatt13@gmail.com>
2022-10-14 19:09:34 +0800

    add package exercise 1 Main.kt add package exercise 1 Main.kt exercise 1 Main.kt

--- src/main/kotlin/activity_01_a/Main.kt
+++ src/main/kotlin/activity_01_a/Main.kt
@@ -3,11 +3,22 @@ import java.time.format.DateTimeFormatter
 
 fun main() {
 
-    var student1:String? = null
-    var student2:String? = null
-    var student3:String? = null
-    var student5:String? = null
-    var student6:String? = null
-    var student7:String? = null
-    var student8:String? = null
+    var student1: String? = null
+    var student2: String? = null
+    var student3: String? = null
+    var student4: String? = null
+    var student5: String? = null
+    var student6: String? = null
+    var student7: String? = null
+    var student8: String? = null
 
+    println("$student1")
+    println("$student2")
+    println("$student3")
+    println("$student4")
+    println("$student5")
+    println("$student6")
+    println("$student7")
+    println("$student8")
+
+}
